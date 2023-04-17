5731cbb2261f53a73816747e1c52218031e8b5b0
ykkwn <monmon_ranma@yahoo.co.jp>
2023-04-17 18:32:03 +0900

    初期化スクリプト（SQLファイル）の設定とエンティティの作成

---
+++ src/main/java/com/techacademy/entity/Authentication.java
@@ -0,0 +1,37 @@
+package com.techacademy.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "authentication")
+public class Authentication {
+
+    /** 主キー。社員番号。20桁。null不許可*/
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(length = 20, nullable = false)
+    private String code;
+
+    /** パスワード。255桁。null不許可*/
+    @Column(length = 255, nullable = false)
+    private String password;
+
+    /** 権限。10桁。null不許可*/
+    @Column(length = 10, nullable = false)
+    private String role;
+
+    /** 従業員テーブルのID（FK）*/
+    @OneToOne
+    @JoinColumn(name="employee_id", referencedColumnName="id")
+    private Employee employee;
+}
