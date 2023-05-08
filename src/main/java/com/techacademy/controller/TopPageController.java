615a45b49e5883e89a83a70c2bb7101d9cd42db8
ykkwn <monmon_ranma@yahoo.co.jp>
2023-05-08 13:28:41 +0900

    @AuthenticationPrincipalの追加（ログインしているユーザー情報を取得）

--- src/main/java/com/techacademy/controller/TopPageController.java
+++ src/main/java/com/techacademy/controller/TopPageController.java
@@ -1,6 +1,9 @@
 package com.techacademy.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TopPageController {
 
     @GetMapping("/")
-    public String getTop() {
+    public String getTop(@AuthenticationPrincipal UserDetails employee, Model model) {
+        model.addAttribute("username", employee.getUsername());
         // topPage.htmlに画面遷移
         return "topPage";
     }
