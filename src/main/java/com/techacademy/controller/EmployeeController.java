df708f92e7d6c349a4bfd4697310a9c1082506d7
ykkwn <monmon_ranma@yahoo.co.jp>
2023-05-11 17:53:32 +0900

    パスワードは更新時のみ入力させる機能の追加。PasswordEncoderの追加（暗号化処理）。

--- src/main/java/com/techacademy/controller/EmployeeController.java
+++ src/main/java/com/techacademy/controller/EmployeeController.java
@@ -2,11 +2,12 @@ package com.techacademy.controller;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,9 @@ import com.techacademy.service.UserDetail;
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     private final EmployeeService service;
     private final AuthenticationService authenticationService;
@@ -39,7 +43,7 @@ public class EmployeeController {
 
     /** Employee登録画面を表示*/
     @GetMapping("/register")
-    public String getRegister(@ModelAttribute Employee employee, @AuthenticationPrincipal UserDetail userDetail, Model model) {
+    public String getRegister(Employee employee, @AuthenticationPrincipal UserDetail userDetail, Model model) {
 
         model.addAttribute("username", userDetail.loginName());
         // employee登録画面に遷移
@@ -49,14 +53,18 @@ public class EmployeeController {
     /** Employee登録処理*/
     @PostMapping("/register")
     public String postRegister(Employee employee, @AuthenticationPrincipal UserDetail userDetail, Model model) {
-        LocalDateTime now = LocalDateTime.now();
-        employee.getAuthentication().setEmployee(employee);
 
         // 社員番号のDB重複を確認
         if(authenticationService.existCode(employee.getAuthentication().getCode())) {
             return getRegister(employee, userDetail, model);
         }
 
+        // パスワードの暗号化
+        String password = employee.getAuthentication().getPassword();
+        employee.getAuthentication().setPassword(passwordEncoder.encode(password));
+
+        LocalDateTime now = LocalDateTime.now();
+        employee.getAuthentication().setEmployee(employee);
         employee.setDeleteFlag(0);
         employee.setCreatedAt(now);
         employee.setUpdatedAt(now);
@@ -68,7 +76,7 @@ public class EmployeeController {
 
     /** Employeeの詳細画面の表示*/
     @GetMapping("/detail/{id}/")
-    public String getDetail(@PathVariable("id") Integer id,@AuthenticationPrincipal UserDetail userDetail, Model model) {
+    public String getDetail(@PathVariable("id") Integer id, @AuthenticationPrincipal UserDetail userDetail, Model model) {
         // Modelに登録
         model.addAttribute("employee", service.getEmployee(id));
         model.addAttribute("username", userDetail.loginName());
@@ -88,11 +96,22 @@ public class EmployeeController {
 
     /** Employee更新処理*/
     @PostMapping("update/{id}")
-    public String postEmployee(Employee employee) {
+    public String postEmployee(Employee employee, @AuthenticationPrincipal UserDetail userDetail, Model model) {
+
+        // パスワードは更新時のみ入力させる機能 + パスワードの暗号化
+        String password = employee.getAuthentication().getPassword();
+        if(password == "") {
+            Employee oldPass = service.getEmployee(employee.getId());
+            password = oldPass.getAuthentication().getPassword();
+        } else {
+            password = passwordEncoder.encode(password);
+        }
+
         LocalDateTime now = LocalDateTime.now();
         employee.getAuthentication().setEmployee(employee);
 
         service.getEmployee(employee.getId());
+        employee.getAuthentication().setPassword(password);
         employee.setDeleteFlag(0);
         employee.setUpdatedAt(now);
         // Employee登録
